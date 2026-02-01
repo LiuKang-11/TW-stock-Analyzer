2ffcadb9714f5837375f2f0d911f0eed6d9fb944
jiashanchang <ivychang1994@gmail.com>
2026-02-01 21:43:18 +0800

    refactor: update shared layout and home components styles, add react-dom & react-router-dom for Recommend page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
 import HomePage from "./pages/HomePage";
+import RecommendPage from "./pages/RecommendPage";
 import "./styles/theme.css";
 import "./styles/button.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomePage />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recommend" element={<RecommendPage />} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
